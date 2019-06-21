a90c859567e80368610259a19ed9288e9198f282
Marco Solazzi <marco.solazzi@gmail.com>
2019-06-21 14:01:14 +0900

    remove size plugin

--- packages/core/tasks/copy.js
+++ packages/core/tasks/copy.js
@@ -1,5 +1,4 @@
 module.exports = (gulp, { pattern = [], dest = '', ...params }, env, api) => {
-  const size = require('gulp-size');
   const { logger, noopStream } = require('../utils');
   const folders = api.pattern(pattern);
   let destFolder;
@@ -18,7 +17,6 @@ module.exports = (gulp, { pattern = [], dest = '', ...params }, env, api) => {
         since: gulp.lastRun(copy),
       })
       .pipe(api.hooks.call('copy:beforeWrite', params['hooks:beforeWrite']))
-      .pipe(size({ title: 'Copy' }))
       .pipe(gulp.dest(destFolder))
       .pipe(api.hooks.call('copy:complete', params['hooks:complete']))
       .pipe(noopStream()); // adds a noop stream to fix this error: https://stackoverflow.com/questions/40098156/what-about-this-combination-of-gulp-concat-and-lazypipe-is-causing-an-error-usin/40101404#40101404
