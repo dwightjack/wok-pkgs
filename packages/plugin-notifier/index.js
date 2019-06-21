18c6660134f5cacc2b161e879228fa49cd777d21
Marco Solazzi <marco.solazzi@gmail.com>
2019-06-21 17:20:57 +0900

    notifier plugin

---
+++ packages/plugin-notifier/index.js
@@ -0,0 +1,21 @@
+const { createPlugin } = require('wok-core/utils');
+const notifier = require('node-notifier');
+const { promisify } = require('util');
+
+const notifyAsync = promisify(notifier.notify.bind(notifier));
+
+function notify(promise, { pkg = {} }, api, { message, title }) {
+  return promise.then(() => {
+    return notifyAsync({
+      title: title || pkg.name || 'application',
+      message,
+      sound: false,
+    });
+  });
+}
+
+module.exports = createPlugin({
+  name: 'notifier',
+  test: ({ enableNotify }) => enableNotify !== false,
+  plugin: notify,
+});
