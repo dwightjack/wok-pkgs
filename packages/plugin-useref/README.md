f6bd073c52960c469b32bcb91378cf250f87860b
Marco Solazzi <marco.solazzi@gmail.com>
2019-09-11 18:13:34 +0900

    docs and refactor for useref plugin

---
+++ packages/plugin-useref/README.md
@@ -0,0 +1,78 @@
+## gulp-useref for Wok
+
+This plugin implements [gulp-useref](https://www.npmjs.com/package/gulp-useref).
+
+| Hook types | Production only | Purpose             |
+| ---------- | --------------- | ------------------- |
+| lazypipe   | yes             | assets optimization |
+
+<!-- TOC -->
+
+- [gulp-useref for Wok](#gulp-useref-for-wok)
+- [Installation](#installation)
+- [Parameters](#parameters)
+- [Usage](#usage)
+  - [Custom Blocks](#custom-blocks)
+
+<!-- /TOC -->
+
+## Installation
+
+This plugin requires `@wok-cli/core`.
+
+```
+npm i @wok-cli/core @wok-cli/plugin-useref --save-dev
+```
+
+## Parameters
+
+Configuration path: `useref`.
+
+| parameter    | type    | default | note             |
+| ------------ | ------- | ------- | ---------------- |
+| `sourcemaps` | boolean | `true`  | write sourcemaps |
+
+All other parameters will be used as gulp-useref [configuration options](https://www.npmjs.com/package/gulp-useref#options).
+
+Note that the `searchPath` and `base` options are enhanced with support for environment templates.
+
+## Usage
+
+The most common usage scenario of this plugin is with [@wok-cli/task-views](#TODO):
+
+```js
+const $ = require('@wok-cli/core');
+const views = require('@wok-cli/task-views');
+const useref = require('@wok-cli/plugin-useref');
+
+const viewsTask = $.task(views, {
+  src: ['src/**/*.html'],
+  dest: 'public',
+});
+
+viewsTask.tap('post', 'useref', useref);
+
+exports.views = viewsTask;
+```
+
+Refer to [gulp-useref](https://www.npmjs.com/package/gulp-useref) documentation for usage details.
+
+### Custom Blocks
+
+gulp-useref allows you to do custom processing via [custom blocks](https://github.com/jonkemp/useref#custom-blocks).
+
+This plugin implements by default a custom `replace` block, which can be used to replace a `script` tag src attribute without processing the source file.
+
+Example input:
+
+```html
+<!-- build:replace /vendors/modernizr.min.js defer -->
+<script src="/vendors/modernizr.js" defer></script>
+<!-- endbuild -->
+```
+
+Output with `--production`:
+
+```html
+<script src="/vendors/modernizr.min.js" defer></script>
+```
--- packages/plugin-useref/index.js
+++ packages/plugin-useref/index.js
@@ -1,45 +1,60 @@
-const { createPlugin, pipeChain, noopStream } = require('@wok-cli/core/utils');
+const { createPlugin } = require('@wok-cli/core/utils');
 
-function userefPlugin(stream, env, api, opts) {
-  const useref = require('gulp-useref');
-  const sourcemaps = require('gulp-sourcemaps');
-  const conf = Object.assign(
-    { sourcemaps: true, processAssets: noopStream },
-    userefPlugin.transforms,
-    opts,
-  );
-
-  conf.searchPath = conf.searchPath && api.pattern(conf.searchPath);
-  conf.base = conf.base && api.resolve(conf.base);
-
-  return stream
-    .pipe(
-      useref,
-      conf,
-      conf.sourcemaps &&
-        pipeChain().pipe(
-          sourcemaps.init,
-          { loadMaps: true },
-        ),
-    )
-    .pipe(
-      conf.processAssets,
-      conf,
-    )
-    .pipe(
-      conf.sourcemaps
-        ? () =>
-            sourcemaps.write(
-              typeof conf.sourcemaps === 'string' ? conf.sourcemaps : undefined,
-            )
-        : noopStream,
-    );
-}
-
-userefPlugin.transforms = {
+const transforms = {
+  /**
+   * Replaces the contained script(s) with the passed-in one.
+   *
+   * @param {string} blockContent Build tag content
+   * @param {string} target Destination URI
+   * @param {string} attrs script attributes
+   * @example
+   * <!-- Replace the development version of modernizr with the production one -->
+   * <!-- build:replace /vendors/modernizr.min.js -->
+   * <script src="/vendors/modernizr.js"></script>
+   * <!-- endbuild -->
+   */
   replace: (blockContent, target, attrs) =>
     `<script src="${target}"${attrs ? ` ${attrs}` : ''}></script>`,
 };
+
+/**
+ * gulp-useref implementation for Wok
+ *
+ * @param {Lazypipe} lazypipe Hook accumulator
+ * @param {object} env Wok environment configuration object
+ * @param {object} api Wok internal API
+ * @param {object} params Plugin parameters. Any non-listed option will be passed to gulp-useref as option.
+ * @param {boolean} [params.sourcemaps=true] Sourcemaps support.
+ * @returns {lazypipe}
+ */
+function userefPlugin(lazypipe, env, api, opts) {
+  const useref = require('gulp-useref');
+  const gulpMaps = require('gulp-sourcemaps');
+
+  const { sourcemaps = true, ...options } = opts;
+
+  options.searchPath = options.searchPath && api.pattern(options.searchPath);
+  options.base = options.base && api.resolve(options.base);
+
+  let ret = lazypipe.pipe(() =>
+    useref(
+      {
+        ...transforms,
+        ...options,
+      },
+      sourcemaps && gulpMaps.init({ loadMaps: true }),
+    ),
+  );
+
+  if (sourcemaps) {
+    ret = ret.pipe(
+      gulpMaps.write,
+      typeof sourcemaps === 'string' ? sourcemaps : undefined,
+    );
+  }
+
+  return ret;
+}
 
 module.exports = createPlugin({
   name: 'useref',
