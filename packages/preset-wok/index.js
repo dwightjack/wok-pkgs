cd1e17830e480551fd791ee7fa050523fc85aeeb
Marco Solazzi <marco.solazzi@gmail.com>
2020-01-06 23:30:59 +0900

    wok preset

---
+++ packages/preset-wok/index.js
@@ -0,0 +1,32 @@
+module.exports = ($) => {
+  const { deploy } = require('@wok-cli/tasks');
+  const ssh = require('@wok-cli/task-ssh');
+  const rsync = require('@wok-cli/plugin-deploy-rsync');
+  const lftp = require('@wok-cli/plugin-deploy-lftp');
+  const standard = require('@wok-cli/preset-standard');
+
+  const preset = standard($);
+
+  preset
+    .globalHook('watcher', 'notifier', require('@wok-cli/plugin-notifier'))
+    .params('watch', {
+      notifier: {
+        message: 'Updated!',
+      },
+    })
+    .set('remote', ssh)
+    .set('$backup', ssh, { command: 'backup' })
+    .set('$sync')
+    .task(deploy)
+    .params({
+      src: '<%= paths.dist.root %>/',
+      exclude: ['.svn*', '.tmp*', '.idea*', '.sass-cache*', '*.sublime-*'],
+    })
+    .hook('strategy', 'rsync', rsync)
+    .hook('strategy', 'lftp', lftp)
+    .end()
+    .set('deploy')
+    .compose((tasks) => $.series(tasks.default, tasks.$backup, tasks.$sync));
+
+  return preset;
+};
